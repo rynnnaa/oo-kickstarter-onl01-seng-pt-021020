c01038c106e6cbb6b4277fcdd9204f85374c3ae6
Ryna Truong <rynatruong@outlook.com>
2020-03-18 18:36:39 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,7 +2,8 @@ class Backer
   attr_accessor
   attr_reader :backed_projects
   
-  def initialize(backed_projects)
+  def initialize(name, backed_projects)
+    @name = name
     @backed_projects = []
   end
 end
