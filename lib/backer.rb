53770f0bce3f792373bc07fd3445ab77a23e2811
Ryna Truong <rynatruong@outlook.com>
2020-03-18 18:40:34 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,7 +7,7 @@ class Backer
     @backed_projects = []
   end
   
-  def back_project(Project)
+  def back_project(new_project)
     new_project = Project.new
     backed_project << new_project
   end
